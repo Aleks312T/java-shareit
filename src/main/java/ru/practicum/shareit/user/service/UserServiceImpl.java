29c9455befd2d581f026e448bfc069e894554af8
Aleks <aleks312t@gmail.com>
2023-07-27 04:36:20 +0300

    feat: начата работа над тестами для UserService

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -89,9 +89,19 @@ public class UserServiceImpl implements UserService {
         log.trace("Завершение вызова метода delete");
     }
 
+    public void checkUserEmail(String email) {
+        log.trace("Вызов метода checkUserEmail с email = {}", email);
+        List<User> sameEmailUsers = userRepository.findByEmailContainingIgnoreCase(email);
+        if (!sameEmailUsers.isEmpty()) {
+            throw new IncorrectParameterException("Электронная почта уже занята");
+        }
+    }
+
     public boolean checkUserEmail(String email, Long id) {
         log.trace("Вызов метода checkUserEmail с email = {}, id = {}", email, id);
         List<User> sameEmailUsers = userRepository.findByEmailContainingIgnoreCase(email);
+        if (sameEmailUsers.isEmpty())
+            return true;
         for (User user : sameEmailUsers) {
             if (!Objects.equals(user.getId(), id)) {
                 return false;
