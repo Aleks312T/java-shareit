b490bf338a6dc57b7d7a7cff5ce51cd6d4a4d299
Aleks <aleks312t@gmail.com>
2023-08-11 01:43:19 +0300

    style: убрана ненужная зависимость

--- gateway/src/main/java/ru/practicum/shareit/item/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemDto.java
@@ -12,11 +12,11 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class ItemDto {
     private Long id;
-    @NotBlank
+
     private String name;
-    @NotBlank
+
     private String description;
-    @NotNull
+
     private Boolean available;
 
     private Long requestId;
